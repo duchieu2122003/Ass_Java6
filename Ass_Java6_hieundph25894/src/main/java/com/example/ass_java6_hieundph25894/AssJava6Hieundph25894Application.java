4f7cfc8c3e75096ae142235fe6a27abec01f5cfb
duchieu2122003 <duchieunguyen2122003@gmail.com>
2023-07-24 15:39:33 +0700

    Commit add project be

---
+++ Ass_Java6_hieundph25894/src/main/java/com/example/ass_java6_hieundph25894/AssJava6Hieundph25894Application.java
@@ -0,0 +1,13 @@
+package com.example.ass_java6_hieundph25894;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssJava6Hieundph25894Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssJava6Hieundph25894Application.class, args);
+    }
+
+}
---
+++ Ass_Java6_hieundph25894/src/test/java/com/example/ass_java6_hieundph25894/AssJava6Hieundph25894ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ass_java6_hieundph25894;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssJava6Hieundph25894ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
